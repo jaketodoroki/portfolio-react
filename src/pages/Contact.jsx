0f03e2d5745a5ccdb8eb88499f0312e22fc21b56
JakeTodoroki <taco7897@gmail.com>
2023-02-01 21:37:23 -0700

    contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,21 @@
 const Contact = () => {
   return ( 
-    <h1>Contact</h1>
+    <div>
+      <h1>Contact</h1>
+      <h3>Reach out if you'd like to work with me!</h3>
+      <p>
+        Link to my
+        <li>
+          <a href="https://github.com/jaketodoroki">Github</a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/jake-todoroki/">LinkedIn</a>
+        </li>
+        <li>
+          <a href="mailto:taco7897@gmail.com">Email</a>
+        </li>
+      </p>
+    </div>
   );
 }
  
